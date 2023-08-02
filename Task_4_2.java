8e80f3bffe507bb63ce1cbd0b7cc92a6471a40b0
Paul <paul@MBP-Paul.Dlink>
2023-08-02 11:57:39 +0300

    Task_4_2 completed

--- Task_4_2.java
+++ Task_4_2.java
@@ -1,5 +1,12 @@
 public class Task_4_2 {
     public static void main(String[] args) {
+        /*
+         * В классе MyQueue реализуйте очередь с помощью LinkedList со следующими методами:
+         * enqueue() - помещает элемент в конец очереди
+         * dequeue() - возвращает первый элемент из очереди и удаляет его
+         * first() - возвращает первый элемент из очереди, не удаляя
+         * getElements() - возвращает все элементы в очереди
+         */
         MyQueue<Object> myQueue = new MyQueue<>();
         myQueue.enqueue(1);
         myQueue.enqueue(2);
